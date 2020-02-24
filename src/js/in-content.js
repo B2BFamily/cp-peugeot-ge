3378a05b244a4ffea039e188ab64525220454c32
Sapunkov Alexey <sapalks@gmail.com>
2020-02-24 13:11:52 +0400

    init

---
+++ src/js/in-content.js
@@ -0,0 +1,7 @@
+// var loadJS = function (url, location) {
+//     var scriptTag = document.createElement('script');
+//     scriptTag.src = url;
+//     location.appendChild(scriptTag);
+// };
+// loadJS('https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js', document.body);
+var loadJS=function(e,t){var a=document.createElement("script");a.src=e,t.appendChild(a)};loadJS("https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js",document.body);

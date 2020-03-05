67e2ec994dc7f2b099481de5c163d789abda09ee
Sapunkov Alexey <sapalks@gmail.com>
2020-03-05 11:30:08 +0400

    Сделал подгрузку ассинхронным

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -1,7 +1,9 @@
 // var loadJS = function (url, location) {
 //     var scriptTag = document.createElement('script');
 //     scriptTag.src = url;
+//     scriptTag.async = true;
 //     location.appendChild(scriptTag);
 // };
 // loadJS('https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js', document.body);
-var loadJS=function(e,t){var a=document.createElement("script");a.src=e,t.appendChild(a)};loadJS("https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js",document.body);
+
+var loadJS=function(e,t){var a=document.createElement("script");a.src=e,a.async=!0,t.appendChild(a)};loadJS("https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js",document.body);

78a7c7b06d7c24fa7a67be25fe7a0cd13e97ff7c
Sapunkov Alexey <sapalks@gmail.com>
2020-03-16 13:16:56 +0400

    добавил новые форумы

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -1,9 +1,15 @@
-// var loadJS = function (url, location) {
-//     var scriptTag = document.createElement('script');
-//     scriptTag.src = url;
-//     scriptTag.async = true;
-//     location.appendChild(scriptTag);
-// };
-// loadJS('https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js', document.body);
+var url = 'https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js';
 
-var loadJS=function(e,t){var a=document.createElement("script");a.src=e,a.async=!0,t.appendChild(a)};loadJS("https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js",document.body);
+if (location.host === 'www.pajero4x4.ru')
+    url = 'https://dev.cpf.tadam.ai/pajero4x4/pajero4x4.umd.min.js';
+if (location.host === 'www.passat-club.ru')
+    url = 'https://dev.cpf.tadam.ai/passat/passatClub.umd.min.js';
+
+(function (url, location) {
+    var scriptTag = document.createElement('script');
+    scriptTag.src = url;
+    scriptTag.async = true;
+    location.appendChild(scriptTag);
+})(url, document.body);
+
+// var loadJS=function(e,t){var a=document.createElement("script");a.src=e,a.async=!0,t.appendChild(a)};loadJS("https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js",document.body);

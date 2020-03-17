c844bdeee1745d99d57150778bbd9fcb45f11d72
Sapunkov Alexey <sapalks@gmail.com>
2020-03-17 17:34:02 +0400

    доработал расширение, что бы работало еще и с 1251 кодировкой на форумах

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -1,15 +1,17 @@
-var url = 'https://dev.cpf.tadam.ai/peugeot/peugeotClub.umd.min.js';
+// var url = 'https://dev.cpf.tadam.ai/peugeot/peugeotClub.umd.min.js';
 
-if (location.host === 'www.pajero4x4.ru')
-    url = 'https://dev.cpf.tadam.ai/pajero4x4/pajero4x4.umd.min.js';
-if (location.host === 'www.passat-club.ru')
-    url = 'https://dev.cpf.tadam.ai/passat/passatClub.umd.min.js';
+// if (location.host.indexOf('pajero4x4.ru') >= 0)
+//     url = 'https://dev.cpf.tadam.ai/pajero4x4/pajero4x4.umd.min.js';
+// if (location.host.indexOf('www.passat-club.ru') >= 0)
+//     url = 'https://dev.cpf.tadam.ai/passat/passatClub.umd.min.js';
 
-(function (url, location) {
-    var scriptTag = document.createElement('script');
-    scriptTag.src = url;
-    scriptTag.async = true;
-    location.appendChild(scriptTag);
-})(url, document.body);
+// (function (url, location) {
+//     var scriptTag = document.createElement('script');
+//     scriptTag.src = url;
+//     scriptTag.async = true;
+//     scriptTag.charset = "utf-8";
+//     scriptTag.type = "text/javascript";
+//     location.appendChild(scriptTag);
+// })(url, document.head);
 
-// var loadJS=function(e,t){var a=document.createElement("script");a.src=e,a.async=!0,t.appendChild(a)};loadJS("https://cpf.tadam.ai/peugeot/peugeotClub.umd.min.js",document.body);
+var url="https://dev.cpf.tadam.ai/peugeot/peugeotClub.umd.min.js";0<=location.host.indexOf("pajero4x4.ru")&&(url="https://dev.cpf.tadam.ai/pajero4x4/pajero4x4.umd.min.js"),0<=location.host.indexOf("www.passat-club.ru")&&(url="https://dev.cpf.tadam.ai/passat/passatClub.umd.min.js"),function(t,a){var e=document.createElement("script");e.src=t,e.async=!0,e.charset="utf-8",e.type="text/javascript",a.appendChild(e)}(url,document.head);
